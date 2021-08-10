2a9ef20bf5cfbc6c87e4d4d0159963f67087a39d
tester101010 <yashik.yashik.2014@mail.ru>
2021-08-11 00:30:40 +0300

    add  <summary>

--- ConsoleLern1App1/Program.cs
+++ ConsoleLern1App1/Program.cs
@@ -10,8 +10,10 @@ namespace ConsoleLern1App1
     {
         static void Main(string[] args)
         {
-            // таблица умножения 5 на 5 без массивов
-            // "this is second branch reserev"
+            ///<summary>
+            /// таблица умножения 5 на 5 без массивов
+            /// "this is second branch reserev"
+            ///</summary>
             Console.WriteLine(" таблица умножения 5 на 5 без массивов");
 
             int X1_1, x1_2, x1_3, x1_4, x1_5;
