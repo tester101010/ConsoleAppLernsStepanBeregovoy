b35732e212f838328696d801eaeb7e9c0a0bb61e
tester101010 <yashik.yashik.2014@mail.ru>
2021-08-10 20:03:58 +0300

    Add project files.

---
+++ ConsoleLern1App1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleLern1App1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // таблица умножения 5 на 5 без массивов
+            Console.WriteLine(" таблица умножения 5 на 5 без массивов");
+
+            int X1_1, x1_2, x1_3, x1_4, x1_5;
+            int X2_1, x2_2, x2_3, x2_4, x2_5;
+            int X3_1, x3_2, x3_3, x3_4, x3_5;
+            int X4_1, x4_2, x4_3, x4_4, x4_5;
+
+            Console.ReadKey();
+        }
+    }
+}
